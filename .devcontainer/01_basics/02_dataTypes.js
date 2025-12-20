92980298a1854136ae4f58d0d0b1d5ce55dfe1df
Dipu Kumar Rajak <work.dipukumar@gmail.com>
2025-12-20 15:10:15 +0000

    dataType

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)// we are using nodejs, not brower
+
+console.log(3
+    +3);
+
+console.log("dipu"); // code readability should me high
+
+let name = "dipu"
+let age = 18 
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/falsecd 01_basics
+// null => standalone vaule
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
+
