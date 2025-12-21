ada60d1632631d811a0de71cb85f28732335e621
Dipu Kumar Rajak <work.dipukumar@gmail.com>
2025-12-21 16:08:06 +0000

    Move basics out of devcontainer

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "hitest"
+const repoCount = 50
+
+//console.log(name + repoCount + " value");
+
+console.log(`hello my name is ${name} and my repo count ${repoCount}`);
+
+const gameName = new String("dipukr")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('p'));
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const anotherString = gameName.slice(-6,2) 
+console.log(anotherString);
+
+const newStringOne = "   dipu   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+
+
+
+
+
+
+
