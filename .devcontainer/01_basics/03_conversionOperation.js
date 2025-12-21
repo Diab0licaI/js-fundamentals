f817e396f7136a42ab71e2fb69fcbef15376cf60
Dipu Kumar Rajak <work.dipukumar@gmail.com>
2025-12-21 05:33:29 +0000

    JS conversion operations

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score =undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+  
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumbee);
+// console.log( valueInNumber);
+ 
+// "33" => 33
+// "33" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ''
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true or 0 => false
+// "" => false
+// "dipu" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber);
+
